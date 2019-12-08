dbc1a7ab0b5d083c3127c6673dafdedab7326772
CalebYapura <52566472+CalebYapura@users.noreply.github.com>
2019-12-08 15:25:28 -0400

    se arreglo o modifico el servico proveedor

--- Cliente/src/app/services/proveedor.service.ts
+++ Cliente/src/app/services/proveedor.service.ts
@@ -1,38 +1,43 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Proveedor } from '../models/proveedor.model';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Proveedor} from '../models/proveedor.model';
+import {HttpClient} from '@angular/common/http';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProveedorService {
-    baseUrl: string;
+  baseUrl: string;
 
-  constructor(protected httpClient:HttpClient) {
+  constructor(protected httpClient: HttpClient) {
 
-      this.baseUrl='http://localhost:3030/proveedor';
-   }
+    this.baseUrl = 'http://localhost:3030/proveedor';
+  }
 
 
+  getAll(): Observable<Proveedor[]> {// este metodo obtiene todo  los artistas
 
-    getAll(): Observable<Proveedor[]> {// este metodo obtiene todo  los artistas
+    return this.httpClient.get<Proveedor[]>(this.baseUrl);
+  }
 
-        return this.httpClient.get<Proveedor[]>(this.baseUrl );
-    }
+  create(proveedor: Proveedor): Observable<any> { // este metodo crea un artista  y recive un  artista por parametro
+    return this.httpClient.post(this.baseUrl, proveedor);
+  }
 
-    create(proveedor: Proveedor): Observable<any> { // este metodo crea un artista  y recive un  artista por parametro
-        return this.httpClient.post(this.baseUrl + '/guardar', Proveedor);
-    }
+  delete(id: number): Observable<Proveedor> { // elimina
+    return this.httpClient.delete<Proveedor>(this.baseUrl + id);
+  }
 
-    delete(id: number): Observable<Proveedor> { // elimina
-        return this.httpClient.delete<Proveedor>(this.baseUrl + '/eliminar/' + id);
-    }
+  update(id: number, proveedor: Proveedor): Observable<any> {// este actualiza
+    // return this.httpClient.put(`${this.baseUrl}/${idArtista}`, artista);
+    return this.httpClient.post(this.baseUrl + id, proveedor);
+    // return this.httpClient.post<Artista>(this.baseUrl + '/actualizar/' + idArtista , artista);
+  }
 
-    update(id: number, proveedor: Proveedor): Observable<any> {// este actualiza
-        // return this.httpClient.put(`${this.baseUrl}/${idArtista}`, artista);
-    return this.httpClient.post(this.baseUrl + '/actualizar/' + id, proveedor);
-        // return this.httpClient.post<Artista>(this.baseUrl + '/actualizar/' + idArtista , artista);
-    }
+  getId(id: string): Observable<any> {
+    return this.httpClient.get<any>(this.baseUrl+id);
+  }
 }
+
+
